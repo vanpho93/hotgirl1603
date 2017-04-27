80cf696d45a2b72429538b9fc0d3e0b8cdbb0cac
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-27 19:56:56 +0700

    style

--- index.js
+++ index.js
@@ -6,11 +6,4 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/', (req, res) => res.render('home'))
-
-// INSERT INTO public."HotGirl"(name, image, "like", dislike)
-// 	VALUES ('Mai Phuong Thuy', '1.jpg', 1, 3),
-//     ('Elly Tran', '2.jpeg', 1, 3),
-//     ('Ngoc Trinh', '3.jpg', 2, 3),
-//     ('Chipu', '4.png', 4, 3),
-//     ('Midu', '5.png', 12, 3)
+app.get('/', (req, res) => res.render('home'));
